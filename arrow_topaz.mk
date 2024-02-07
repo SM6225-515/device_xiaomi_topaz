a19194f075b43e74865a3ceba09361b22cb43ad4
Ctps123 <79877353+Ctps123@users.noreply.github.com>
2024-02-06 23:08:20 -0300

    Update and rename lineage_topaz.mk to arrow_topaz.mk

--- arrow_topaz.mk
+++ arrow_topaz.mk
@@ -12,9 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/topaz/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := lineage_topaz
+PRODUCT_NAME := arrow_topaz
 PRODUCT_DEVICE := topaz
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
