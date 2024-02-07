39012223111fe427b8433a4c43a8f3db972ab261
Ctps123 <79877353+Ctps123@users.noreply.github.com>
2024-02-06 23:06:13 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_topaz.mk
+    $(LOCAL_DIR)/arrow_topaz.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_topaz-user \
-    lineage_topaz-userdebug \
-    lineage_topaz-eng
+    arrow_topaz-user \
+    arrow_topaz-userdebug \
+    arrow_topaz-eng
